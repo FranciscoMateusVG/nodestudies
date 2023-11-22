6cb55fbdb0809300600dba99c2f4fd0bd574ee9f
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2023-11-21 21:28:32 -0300

    fifth

---
+++ chapter4/spider3(sequential flow)/spider-cli.mjs
@@ -0,0 +1,15 @@
+import { spider } from './webspider3.mjs'
+
+const url = process.argv[2]
+const nesting = Number.parseInt(process.argv[3], 10) || 1
+
+spider(url, nesting, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+
+  console.log('Download complete')
+})
+
+// nodemon spider-cli.mjs https://www.construtoraquebec.com.br/index.html 1
---
+++ chapter4/spider3(sequential flow)/spiderLinks.mjs
@@ -0,0 +1,27 @@
+import { getPageLinks } from '../utils.mjs'
+import { spider } from './webspider3.mjs'
+
+export function spiderLinks(currentUrl, body, nesting, cb) {
+  if (nesting === 0) return process.nextTick(cb)
+
+  const links = getPageLinks(currentUrl, body)
+
+  if (links.length === 0) return process.nextTick(cb)
+
+  // This just goes rambo mode in parallel
+  for (const link of links) {
+    spider(link, nesting - 1, cb)
+  }
+
+  // This goes sequential
+  //   iterate(0, links, nesting, cb)
+}
+
+function iterate(index, links, nesting, cb) {
+  if (index === links.length) return cb()
+
+  spider(links[index], nesting - 1, (err) => {
+    if (err) return cb(err)
+    iterate(index + 1, links, nesting, cb)
+  })
+}
